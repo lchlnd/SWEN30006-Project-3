88c1bf8de740dd71a8de3e631c6306a444c7316f
Nathan Malishev <nathan.malishev@gmail.com>
2015-05-19 16:29:05 +1000

    Seeds positions/postcodes/locations & links them all

---
+++ Project3/config/initializers/task_scheduler.rb
@@ -0,0 +1,26 @@
+# require 'rubygems'
+# require 'rufus/scheduler'
+
+
+# require 'scrapers/bom'
+# require 'scrapers/darksky'
+
+
+
+# scheduler = Rufus::Scheduler.new
+
+
+
+
+
+# #do every 30 minutes
+# scheduler.every '45s', :first_in =>'3s'  do
+	
+# 	#run bom scraper
+	
+	
+	
+# 	#run darksky scraper
+
+# end
+
---
+++ Project3/lib/scrapers/bom.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+
+
+class ScrapeBOM
+
+	
