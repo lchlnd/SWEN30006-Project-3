4b4440f7e7c5319107abe366f6781b7b2922f348
rokane <ryanokane@ua-unistudent-ten-9-177-210.uniaccess.unimelb.edu.au>
2015-05-27 23:28:47 +1000

    Views Updated

--- Project3/app/controllers/weather_controller.rb
+++ Project3/app/controllers/weather_controller.rb
@@ -64,31 +64,22 @@ class WeatherController < ApplicationController
 		end
 	end
 
+	def find_postcode_data
 
-	# Need to find a way to combine this with the above data method i.e. to distinguish between whether the parameter is
-	# postcode or a location_id
-	def postcode_data
-		# Pretend a postcode and date have been given:
-		postcode = 3525
-		date = Date.today
-
-
-		p = Postcode.find_by_code(postcode)
-		@locations = p.locations
-
-		# Prepare data for JSON builder:
-		data = {}
-		@locations.each do |l|
-			puts "a location?"
-			readings = Reading.where(:location_id => l.id, :created_at => date)
-			data[l] = readings
-		end
+		@locations = Location.all
 
 		respond_to do |format|
-			format.html
-			format.js
-			format.json {render json: build_postcode_readings(date, data)}
-		end
 
+			format.html {render "find_postcode_data"}
+		end
 	end
+
+	def find_location_data
+
+		respond_to do |format|
+
+			format.html {render "find_location_data"}
+		end
+	end
+
 end
--- Project3/config/routes.rb
+++ Project3/config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   get 'weather/locations' 
   get 'weather/data/:id/:date', to: 'weather#data'
-
-  # Only temporary:
-  get 'weather/postcode_data'
+  get 'weather/find_postcode_data'
+  get 'weather/find_location_data'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'weather#locations'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
