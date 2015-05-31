f128e0a617845504fa4d065d5491d3ae5f7288b2
Lochlan Brick <lochiebrick@gmail.com>
2015-05-31 22:34:06 +1000

    predictions fixed, logo added

--- Project3/config/initializers/task_scheduler.rb
+++ Project3/config/initializers/task_scheduler.rb
@@ -3,7 +3,7 @@ require 'rufus/scheduler'
 require 'rake'
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '10m' do
+scheduler.every '10m', :first_in=>'3s' do
   system('rake weather:scrape_bom')
 end
 
