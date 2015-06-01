ea08b5799ee4b5a226139806f8a12794514e1859
Lachlan Dee <dee.lachlan@gmail.com>
2015-06-01 16:39:00 +1000

    Found and fixed bug where location view will break if wind direction is nil, hadn't handled in scraper

--- Project3/app/models/wind_direction.rb
+++ Project3/app/models/wind_direction.rb
@@ -5,7 +5,12 @@ class WindDirection < Datapoint
 		map = {0.0 => 'N', 22.5 => 'NNE', 45.0 => 'NE', 67.5 => 'ENE', 90.0 => 'E', 112.5 => 'ESE', 135.0 => 'SE', 
 			157.5 => 'SSE', 180.0 => 'S', 202.5 => 'SSW', 225.0 => 'SW', 247.5 => 'WSW', 270.0 => 'W', 292.5 => 'WNW',
 			315.0 => 'NW', 337.5 => 'NNW', 360.0 => 'N'}
-		map[(self.value/22.5).round*22.5]
+
+    if self.value.nil?
+      '-'
+    else
+      map[(self.value/22.5).round*22.5]
+    end
 	end
 
 	# Converts the given compass bearing in string form into a float value in degrees
